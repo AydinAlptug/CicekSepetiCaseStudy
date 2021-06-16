66b2a763982a728830e8faf573e521228f5396b8
AydinAlptug <yusufaydinceng@gmail.com>
2021-06-16 19:54:20 +0300

    cart service test last

--- CicekSepetiCaseStudy.Test/CartServiceTest.cs
+++ CicekSepetiCaseStudy.Test/CartServiceTest.cs
@@ -105,5 +105,78 @@ namespace CicekSepetiCaseStudy.Test
                 Assert.AreEqual(cartService.AddProductToCartAsync(test_product).Result, null);
             }
         }
+
+        [TestMethod]
+        public void UpdateProductInCartTest()
+        {
+            int test_amount = 20;
+            int test_id = 913;
+            BasketProduct test_product;
+
+            var options = new DbContextOptionsBuilder<CartDbContext>()
+                             .UseInMemoryDatabase("test_cart_db_4")
+                             .Options;
+            using (var context = new CartDbContext(options))
+            {
+                test_product = new BasketProduct
+                {
+                    productId = test_id,
+                    basketProductGuid = Guid.NewGuid(),
+                    productName = "SomeName",
+                    description = "SomeDescription",
+                    price = (float)100,
+                    amount = 1
+                };
+
+                context.Cart.Add(test_product);
+                context.SaveChanges();
+            }
+            using (var context = new CartDbContext(options))
+            {
+                test_product.amount = test_amount;
+                
+                CartService cartService = new CartService(context);
+                Assert.AreEqual(cartService.UpdateProductInCart(test_product).Result, 1);
+                Assert.AreEqual(cartService.GetProductById(test_product.productId).Result.amount, test_amount);
+                Assert.AreNotEqual(cartService.GetProductById(test_product.productId).Result.amount, -1);
+            }
+        }
+
+        [TestMethod]
+        public void RemoveProductFromCart()
+        {
+            int test_amount = 20;
+            int test_id = 913;
+            BasketProduct test_product;
+
+            var options = new DbContextOptionsBuilder<CartDbContext>()
+                             .UseInMemoryDatabase("test_cart_db_4")
+                             .Options;
+            using (var context = new CartDbContext(options))
+            {
+                test_product = new BasketProduct
+                {
+                    productId = test_id,
+                    basketProductGuid = Guid.NewGuid(),
+                    productName = "SomeName",
+                    description = "SomeDescription",
+                    price = (float)100,
+                    amount = 1
+                };
+
+                context.Cart.Add(test_product);
+                context.SaveChanges();
+            }
+            using (var context = new CartDbContext(options))
+            {
+                test_product.amount = test_amount;
+
+                CartService cartService = new CartService(context);
+                Assert.AreEqual(cartService.RemoveProductFromCart(test_product).Result, 1);
+
+                Assert.AreEqual(cartService.GetAllProducts().Result.Count(), 0);
+                Assert.AreNotEqual(cartService.GetAllProducts().Result.Count(), 1);
+            }
+        }
     }
 }
